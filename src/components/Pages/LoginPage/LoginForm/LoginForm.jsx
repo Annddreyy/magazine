7de3b8d8f794333ce5779f742e74d564884bf999
Andrey22 <andreymail22112006@gmail.com>
2025-04-26 20:35:26 +0300

    fix: update login form

--- src/components/Pages/LoginPage/LoginForm/LoginForm.jsx
+++ src/components/Pages/LoginPage/LoginForm/LoginForm.jsx
@@ -6,18 +6,18 @@ const LoginForm = () => {
     return (
         <div className={ classes.form }>
             <div className={ classes.left }>
-                <h2>Регистрация</h2>
-                <p>Нет аккаунта</p>
-                <NavLink to={'/registration'}>Зарегистрироваться</NavLink>
+                <h2 className={ classes.formPartTitle }>Регистрация</h2>
+                <p>Нет аккаунта?</p>
+                <NavLink to={'/registration'} className={ classes.link }>Зарегистрироваться</NavLink>
             </div>
             <div>
-                <h2>Авторизация</h2>
+                <h2 className={ classes.formPartTitle }>Авторизация</h2>
                 <form action="" method="get" className={ classes.items }>
                     <label htmlFor='login'>Логин <span className={ classes.redStar }>*</span></label>
                     <Input type='text' />
                     <label htmlFor='password'>Пароль <span className={ classes.redStar }>*</span></label>
                     <Input type='password' />
-                    <Input type='submit' />
+                    <button className={ classes.button }>Войти</button>
                 </form>
             </div>
         </div>
