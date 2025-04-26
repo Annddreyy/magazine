49d07887080bef288dfe237e61558721ad685b88
Andrey22 <andreymail22112006@gmail.com>
2025-04-26 22:19:25 +0300

    feat: add styles for contacts page

---
+++ src/components/Pages/ContactsPage/ContactsMain/ContactsMain.jsx
@@ -0,0 +1,23 @@
+import Addresses from "./../../../../components/Pages/ContactsPage/Addresses/Addresses";
+import ContactWithUs from "./../../../../components/Pages/ContactsPage/ContactWithUs/ContactWithUs";
+import WorkingHours from "./../../../../components/Pages/ContactsPage/WorkingHours/WorkingHours";
+import classes from './ContactsMain.module.css';
+import img from './../../../../assets/images/product3.png';
+
+const ContactsMain = () => {
+    return (
+        <section className='container'>
+            <h2>Контакты</h2>
+            <div className={ classes.items }>
+                <div className={ classes.text }>
+                    <Addresses />
+                    <WorkingHours />
+                    <ContactWithUs />
+                </div>
+                <img src={ img } alt="" />
+            </div>
+        </section>
+    )
+};
+
+export default ContactsMain;
--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,9 +1,6 @@
 import HeaderContainer from './../components/common/Header/HeaderContainer';
 import FooterContainer from './../components/common/Footer/FooterContainer';
-
-import Addresses from "./../components/Pages/ContactsPage/Addresses/Addresses";
-import ContactWithUs from "./../components/Pages/ContactsPage/ContactWithUs/ContactWithUs";
-import WorkingHours from "./../components/Pages/ContactsPage/WorkingHours/WorkingHours";
+import ContactsMain from '../components/Pages/ContactsPage/ContactsMain/ContactsMain';
 
 const Contacts = () => {
     return (
@@ -12,16 +9,6 @@ const Contacts = () => {
             <ContactsMain />
             <FooterContainer />
         </div>
-    )
-};
-
-const ContactsMain = () => {
-    return (
-        <main>
-            <Addresses />
-            <WorkingHours />
-            <ContactWithUs />
-        </main>
     )
 };
 
