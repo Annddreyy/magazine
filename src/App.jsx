e4b685ccf1ddafaffd2c7116e22e14356e41e695
Andrey22 <andreymail22112006@gmail.com>
2025-04-26 12:28:13 +0300

    feat: add about page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Main from './pages/Main';
 import Catalog from './pages/Catalog';
 import Contacts from './pages/Contacts';
+import About from './pages/About';
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
             <Route path='/' element={ <Main /> } />
             <Route path='/catalog' element={ <Catalog /> } />
             <Route path='/contacts' element={ <Contacts /> } />
+            <Route path='/about' element={ <About /> } />
         </Routes>
     )
 }
---
+++ src/pages/About.jsx
@@ -0,0 +1,13 @@
+import HeaderContainer from './../../src/components/Header/HeaderContainer';
+import FooterContainer from './../../src/components/Footer/FooterContainer';
+
+const About = () => {
+    return (
+        <div>
+            <HeaderContainer />
+            <FooterContainer />
+        </div>
+    )
+};
+
+export default About;
