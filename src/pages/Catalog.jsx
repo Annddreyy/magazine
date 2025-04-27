4a98a0d2251d2e80166689166e36db1afd93e9db
Andrey22 <andreymail22112006@gmail.com>
2025-04-27 16:47:49 +0300

    feat: update favority page

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,10 +1,12 @@
 import HeaderContainer from './../components/common/Header/HeaderContainer';
 import FooterContainer from './../components/common/Footer/FooterContainer';
+import ProductsContainer from '../components/common/Products/ProductsContainer';
 
 const Catalog = () => {
     return (
         <div>
             <HeaderContainer />
+            <ProductsContainer />
             <FooterContainer />
         </div>
     )
--- src/pages/Favority.jsx
+++ src/pages/Favority.jsx
@@ -1,11 +1,12 @@
 import HeaderContainer from './../components/common/Header/HeaderContainer';
 import FooterContainer from './../components/common/Footer/FooterContainer';
+import ProductsContainer from '../components/common/Products/ProductsContainer';
 
 const Favority = () => {
     return (
         <div>
             <HeaderContainer />
-            
+            <ProductsContainer />
             <FooterContainer />
         </div>
     )
