9ac35f5dd14f2dff6e70eb38315e6316105df182
Andrey22 <andreymail22112006@gmail.com>
2025-05-01 14:16:20 +0300

    feat: add react.suspense

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,20 @@ import { compose } from 'redux';
 import { connect } from 'react-redux';
 import './App.css';
 import Main from './pages/Main';
-import Catalog from './pages/Catalog';
-import Contacts from './pages/Contacts';
-import About from './pages/About';
-import Reviews from './pages/Reviews';
-import Bin from './pages/Bin';
-import Favority from './pages/Favority';
-import Login from './pages/Login';
-import Product from './pages/Product';
-import Registration from './pages/Registration';
 import { checkUser } from './redux/auth/authThunks';
 import { withRouter } from './hoc/withRouter';
 import { setLastPage } from './redux/app/appReducer';
-import HeaderContainer from './components/common/Header/HeaderContainer';
+import Loading from './components/common/Loading/Loading';
+
+const Catalog = React.lazy(() => import('./pages/Catalog'));
+const Contacts = React.lazy(() => import('./pages/Contacts'));
+const About = React.lazy(() => import('./pages/About'));
+const Reviews = React.lazy(() => import('./pages/Reviews'));
+const Bin = React.lazy(() => import('./pages/Bin'));
+const Favority = React.lazy(() => import('./pages/Favority'));
+const Login = React.lazy(() => import('./pages/Login'));
+const Product = React.lazy(() => import('./pages/Product'));
+const Registration = React.lazy(() => import('./pages/Registration'));
 
 class App extends React.Component {
     componentDidMount() {
@@ -36,15 +37,51 @@ class App extends React.Component {
         return (
             <Routes>
                 <Route path='/' element={ <Main /> } />
-                <Route path='/catalog' element={ <Catalog /> } />
-                <Route path='/contacts' element={ <Contacts /> } />
-                <Route path='/about' element={ <About /> } />
-                <Route path='/reviews' element={ <Reviews /> } />
-                <Route path='/bin' element={ <Bin /> } />
-                <Route path='/favorites' element={ <Favority /> } />
-                <Route path='/login' element={ <Login /> } />
-                <Route path='/registration' element={ <Registration /> } />
-                <Route path='/product/:productId' element={ <Product /> } />
+                <Route path='/catalog' element={ 
+                    <React.Suspense fallback={<Loading />}>
+                        <Catalog />
+                    </React.Suspense>
+                }/>
+                <Route path='/contacts' element={ 
+                    <React.Suspense fallback={<Loading />}>
+                        <Contacts />
+                    </React.Suspense>
+                }/>
+                <Route path='/about' element={ 
+                    <React.Suspense fallback={<Loading />}>
+                        <About />
+                    </React.Suspense>
+                }/>
+                <Route path='/reviews' element={ 
+                    <React.Suspense fallback={<Loading />}>
+                        <Reviews />
+                    </React.Suspense>
+                }/>
+                <Route path='/bin' element={ 
+                    <React.Suspense fallback={<Loading />}>
+                        <Bin />
+                    </React.Suspense>
+                }/>
+                <Route path='/favorites' element={ 
+                    <React.Suspense fallback={<Loading />}>
+                        <Favority />
+                    </React.Suspense>
+                }/>
+                <Route path='/login' element={ 
+                    <React.Suspense fallback={<Loading />}>
+                        <Login />
+                    </React.Suspense>
+                }/>
+                <Route path='/registration' element={ 
+                    <React.Suspense fallback={<Loading />}>
+                        <Registration />
+                    </React.Suspense>
+                }/>
+                <Route path='/product/:productId' element={ 
+                    <React.Suspense fallback={<Loading />}>
+                        <Product />
+                    </React.Suspense>
+                }/>
             </Routes>
         )
     }
