a94b579be6ced7768f3e0d5a6d3cebfb0f0d2132
Andrey22 <andreymail22112006@gmail.com>
2025-05-02 16:32:00 +0300

    feat: update links

--- src/config/links.js
+++ src/config/links.js
@@ -12,6 +12,6 @@ export const links = [
     { id: 6, link: '/bin', text: 'Корзина', elem: [Elements.HEADER, Elements.FOOTER] },
     { id: 7, link: '/favorites', text: 'Избранное', elem: [Elements.HEADER, Elements.FOOTER]  }, 
     { id: 8, link: '/login', text: 'Войти', elem: [Elements.HEADER], isRightLink: true },
-    { id: 9, link: '/confidentiality', text: 'Политика конфиденциальности', elem: [Elements.FOOTER], isRightLink: true },
-    { id: 10, link: '/personal_data', text: 'Политика обработки персональных данных', elem: [Elements.FOOTER], isRightLink: true }
+    { id: 9, link: 'https://docs.google.com/document/d/1l2z6ZJv1pCC8bsN3dFJrtdddgQCRHiRMk_dMWpU0a5Q/edit?tab=t.0#heading=h.lu913s84dd7t', text: 'Политика конфиденциальности', elem: [Elements.FOOTER], isRightLink: true },
+    { id: 10, link: 'https://drive.google.com/file/d/1-poWVvFrnM0BAfZSB4FrzXbAbi3affwE/view?usp=drive_link', text: 'Инструкция по использованию', elem: [Elements.FOOTER], isRightLink: true }
 ];
