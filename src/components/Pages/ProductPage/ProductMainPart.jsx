536c6a86b52f4bf3e3286d95f3bb672d95ae5f50
Andrey22 <andreymail22112006@gmail.com>
2025-05-10 23:30:07 +0300

    fix: fix bug with photo

--- src/components/Pages/ProductPage/ProductMainPart.jsx
+++ src/components/Pages/ProductPage/ProductMainPart.jsx
@@ -1,12 +1,13 @@
 import classes from './ProductMainPart.module.css';
 import defaultImg from './../../../assets/images/Card/product.png';
+import choosePhoto from '../../../utils/choosePhoto';
 
-const ProductMainPart = ({ title, description, producer, img, compound }) => {
+const ProductMainPart = ({ title, description, producer, imgSrc, compound }) => {
     return (
         <section className='container'>
             <h2>{ title }</h2>
             <div className={ classes.description }>
-                <img src={ img || defaultImg } className={ classes.img } alt="" />
+                <img src={ choosePhoto(imgSrc, defaultImg, 'https://raw.githubusercontent.com/Annddreyy/magazine-images/refs/heads/master/products/') } className={ classes.img } alt="" />
                 <div className={ classes.text }>
                     <p className={ classes.title }>Описание:</p>
                     <p>{ description || 'Описание отсутствует' }</p>
